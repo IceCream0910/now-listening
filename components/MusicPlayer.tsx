b02019b5554f08edc5eb842ebe4b239ce0a84e99
icecream0910 <taein2370@naver.com>
2024-08-20 04:42:46 +0900

    improve: 음원 검색 query 수정

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -109,7 +109,7 @@ export default function MusicPlayer() {
     if (!player) return;
 
     const fetchVideoId = async () => {
-      const response = await fetch(`/api/search?query=${currentMusic.title} ${currentMusic.artist} audio official`);
+      const response = await fetch(`/api/search?query=${currentMusic.title} ${currentMusic.artist} auto-generated`);
       const result = await response.json();
       setCurrentVideoId(result.data.id);
       console.log(result.data.id);
